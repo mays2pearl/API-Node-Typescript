5ab5c02c50675824b7da29fb920d6d2922c74801
mays2pearl <maysa.binho.perola@hotmail.com>
2023-07-22 12:16:21 -0300

    feature:add config estilos de codigo

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
-import { server  } from './server/Server';
+import { server  } from './server/Server'; 
 
-server.listen(3333, () => console.log('App rodando!!!!!'));
+server.listen(3333, () => console.log('App rodando!!!!!'));//aqui passamos como parametro a porta que roda a api e como callback colocamos um console.log
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,7 +1,8 @@
-/* eslint-disable linebreak-style */
 import express from 'express'; //aqui importamos tudo que é disponivel no expresse, e fica salvo dentro de express
 
 const server = express(); //criamos instacia basica do servidor
+
+
 server.get('/', (req, res) => { //construimos a rota de get com uma função que recebe 2 parametros
 
 	return res.send('Hello, world!');//aqui dentro temos o retorno do servidor
